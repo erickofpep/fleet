7e124fe5a90d555fc64c423567d983a23e07f38f
erickofpep <ericpeprah@gmail.com>
2024-04-18 13:24:53 +0100

    edited CarOwnersLoginController

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,10 @@ use App\Http\Controllers\CarOwnersLoginController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::post('/carowners_login',
-    [App\Http\Controllers\CarOwnersLoginController::class, 'login'])->name('carowners_login');
-
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/carowners_login',
+    [App\Http\Controllers\CarOwnersLoginController::class, 'login'])->name('carowners_login');
