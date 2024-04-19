8b582842019c9b2a2e5cfc510348832018c522fd
erickofpep <ericpeprah@gmail.com>
2024-04-19 21:08:46 +0100

    BidsController edited

--- app/Http/Controllers/BidsController.php
+++ app/Http/Controllers/BidsController.php
@@ -5,16 +5,37 @@ namespace App\Http\Controllers;
 use App\Models\Bids;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 class BidsController extends Controller
 {
     public function bids(Request $request){
 
-        $Expected_States = array("accepted", "declined");
+    $Expected_States = array("accepted", "declined");
 
-        $checkIfDriverIDExist = DB::table('driver')->where('id', $request->driver_id)->first();
+if( Schema::hasTable('driver') ){
+    $checkIfDriverIDExist = DB::table('driver')->where('id', $request->driver_id)->first();
+}
+else{
+    return json_encode([
+        '500'=>'error',
+        'message'=>'Drivers table does not exist'
+    ], JSON_PRETTY_PRINT);
+}
 
-        $checkIfCarIDExist = DB::table('vehicle')->where('id', $request->vehicle_id)->first();
+if( Schema::hasTable('vehicle') ){
+    $checkIfCarIDExist = DB::table('vehicle')->where('id', $request->vehicle_id)->first();
+}
+else{
+
+    return json_encode([
+        '500'=>'error',
+        'message'=>'Vehicles table does not exist'
+    ], JSON_PRETTY_PRINT);
+}
+
+$checkIfBidExists=DB::table('bids')->where('driver_id', $request->driver_id)->where('vehicle_id', $request->vehicle_id)->first();
+
 
         if( empty($request->driver_id) || $request->driver_id=='' ){
             return json_encode([
@@ -27,6 +48,13 @@ class BidsController extends Controller
                 return json_encode([
                     '500'=>'error',
                     'message'=>'vehicle_id is required'
+                ], JSON_PRETTY_PRINT);
+
+            }
+            elseif( !empty($request->bid_status) && ( !array_key_exists('accepted', $Expected_States) || !array_key_exists('declined', $Expected_States) ) ){
+                return json_encode([
+                    '500'=>'error',
+                    'message'=>'bid_status should be either accepted or declined'
                 ], JSON_PRETTY_PRINT);
 
             }
@@ -46,12 +74,22 @@ class BidsController extends Controller
                 ], JSON_PRETTY_PRINT);
 
             }
+            // /*
+            elseif( $checkIfBidExists && $checkIfBidExists->bid_status !='expired' ){
+
+                return json_encode([
+                    '500'=>'error',
+                    'message'=>'duplicate bid'
+                ], JSON_PRETTY_PRINT);
+
+            }
+            // */
             else{
 
                 $userComment = new Bids();
-                $userComment->name = $request->input('driver_id');
-                $userComment->email = $request->input('vehicle_id');
-                $userComment->message = $request->input('bid_status');
+                $userComment->driver_id = $request->input('driver_id');
+                $userComment->vehicle_id = $request->input('vehicle_id');
+                $userComment->bid_status = $request->input('bid_status');
                 $userComment->save();
 
                 return json_encode([
